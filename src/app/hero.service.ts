9e26b880ec70825055fe1ead6cde81e0434a5bd8
TBSergio <TBSergio@walla.com>
2018-01-14 22:04:58 +0200

    Changed hero-service to work async

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,14 @@
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
 
 @Injectable()
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
   constructor() { }
